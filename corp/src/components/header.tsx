254db05942c4d7af12449b06afc1432eb70c0da6
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-11-30 13:45:21 +0000

    Hero component

--- corp/src/components/header.tsx
+++ corp/src/components/header.tsx
@@ -3,11 +3,17 @@ import React from "react";
 
 function header() {
   return (
-    <header>
-      <Link href="/">Home</Link>
-      <Link href="/performance">Performance</Link>
-      <Link href="/reliability">Reliability</Link>
-      <Link href="/scale">Scale</Link>
+    <header className="w-full absolute text-white z-10">
+      <nav className="container relative flex flex-wrap items-center justify-between mx-auto p-8">
+        <div className="font-bold text-3xl">
+          <Link href="/">Home</Link>
+        </div>
+        <div className="space-x-4 text-xl">
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
+          <Link href="/scale">Scale</Link>
+        </div>
+      </nav>
     </header>
   );
 }
