7fc0cfb0d8814a3dab57d33cc55c3e594663390a
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-12-04 11:32:31 +0000

    Loading page

--- snippets/src/app/snippets/[id]/not-found.tsx
+++ snippets/src/app/snippets/[id]/not-found.tsx
@@ -4,7 +4,7 @@ function SnippetNotFound() {
   return (
     <div>
       <h1 className="text-xl bold">
-        Sorry, but we couldn&apos;t find that particular snippet{" "}
+        Sorry, but we couldn&apos;t find that particular snippet.
       </h1>
     </div>
   );
