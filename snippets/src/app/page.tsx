edfe2a957df86cf361b3695ea9d7481d99eb64a1
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-12-04 10:26:08 +0000

    fetching data

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div>Home page</div>;
+import { db } from "@/db";
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <div key={snippet.id}>
+        <h3 className="font-bold m-3">{snippet.title}</h3>
+        <pre className="border rounded p-2">{snippet.code}</pre>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1 className="font-bold text-2xl m-3">Snippets</h1>
+      {renderedSnippets}
+    </div>
+  );
 }
