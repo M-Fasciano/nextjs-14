2499c20c1051cfff61384c8b9e0d5fa1b451c122
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-12-08 09:14:29 +0000

    on demand caching

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -2,6 +2,7 @@
 
 import { db } from "@/db";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export async function editSnippet(id: number, code: string) {
   await db.snippet.update({
@@ -15,6 +16,7 @@ export async function editSnippet(id: number, code: string) {
 export async function deleteSnippet(id: number) {
   await db.snippet.delete({ where: { id } });
 
+  revalidatePath("/");
   redirect("/");
 }
 
@@ -58,6 +60,7 @@ export async function createSnippet(
     }
   }
 
+  revalidatePath("/");
   // Redirect the user back to the root route
   redirect("/");
 }
