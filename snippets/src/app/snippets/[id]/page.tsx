3f278394db5e3e7c54bd776b82779cf2cbceb8f2
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-12-11 08:38:52 +0000

    Caching dynamic routes

--- snippets/src/app/snippets/[id]/page.tsx
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -44,3 +44,15 @@ async function SnippetShowPage(props: SnippetShowPageProps) {
 }
 
 export default SnippetShowPage;
+
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany();
+
+  return snippets.map((snippet) => {
+    return {
+      params: {
+        id: snippet.id.toString(),
+      },
+    };
+  });
+}
